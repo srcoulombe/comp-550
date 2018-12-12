5c5f94dcdeef3f288effd1badfbfb41e9cb8e919
Woo Hyeok Kang <wkang@mit.edu>
2018-12-12 11:54:32 -0500

    Uploading script to learn parameters via matlab minka implementation of polya. Speed improvement is about 10x via Matlab. Manually learning parameters and loading in python

---
+++ matlab/fastfit/learnParametersScript.m
@@ -0,0 +1,30 @@
+% script to learn alphas
+totalSplitNum = 2;
+
+testCluster = 'twenty_newsgroups';
+isTwentyNewsgroup = strcmp( testCluster, 'twenty_newsgroups' );
+numTopics = 20;
+dataDir = '../../training_testing_and_results_pickles/';
+fNameFormat = strcat( dataDir, testCluster, '/cv%d/', testCluster, '_training_matrix_cv#%d.mat' );
+learntParamFormat = strcat( dataDir, testCluster, '/cv%d/', testCluster, '_trained_param_cv#%d.mat' );
+for splitIdx = 1:totalSplitNum
+    splitIdx = splitIdx - 1; 
+    fName = sprintf( fNameFormat, splitIdx, splitIdx );
+    learntFName = sprintf( learntParamFormat, splitIdx, splitIdx );
+    trainingData = load( fName );
+    % fields are 'arr' + topic#
+    
+    % generate initial alpha, and variable-indexable name for imported .mat
+    % file
+    sz = size( trainingData.arr0 );
+    alphaInit = ones( 1, sz(2) );
+    topicDataMFns = fieldnames( trainingData );
+    
+    alphaLearntM = zeros( numTopics, sz(2) );
+    for topicIdx = 1:numTopics
+        trainingDataM = trainingData.( topicDataMFns{ topicIdx } );
+        alphaLearntM( topicIdx, : ) = polya_fit_simple( trainingDataM, alphaInit );
+    end
+    save( learntFName, alphaLearntM );
+        
+end
